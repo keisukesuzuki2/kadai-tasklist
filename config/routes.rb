ab86fe43cf898616ffb4a29c5e57ce49c4d4387a
keisuke.suzuki2 <keisuke.suzuki02@g.softbank.co.jp>
2018-10-13 06:49:58 +0000

    fixed bootstrap & making user pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
 
+    get 'signup', to: 'users#new'
+    resources :users, only: [:index, :show, :new, :create]
+
+    get 'signup', to: 'users#edit'
+    resources :users, only: [:index, :show, :new, :create]
+
+    get 'signup', to: 'users#show'
+    resources :users, only: [:index, :show, :new, :create]
+
     resources :tasks
+    
 end
